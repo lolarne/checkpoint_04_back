cfc11a436787d8274b342fc2c6c678c6d797f461
lolarne <rne.lola@gmail.com>
2021-02-03 13:47:25 +0100

    add galeries routes to get + filter + admin

--- src/routes/galerie.routes.js
+++ src/routes/galerie.routes.js
@@ -1,4 +1,70 @@
-const {connection} = require('../db_connection');
+const { connection } = require('../db_connection');
 const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    const sql = "SELECT * FROM illustration";
+    connection.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).send({ errorMessage: 'Cannot get the illustrations' });
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+
+router.get('/oldest', (req, res) => {
+    const sql = "SELECT * FROM illustration ORDER BY date_creation ASC";
+    connection.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).send({ errorMessage: 'Cannot get the oldest illustrations' });
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+
+router.get('/latest', (req, res) => {
+    const sql = "SELECT * FROM illustration ORDER BY date_creation DESC";
+    connection.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).send({ errorMessage: 'Cannot get the latest illustrations' });
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+
+router.get('/:color/:category/:material', (req, res) => {
+    const sql = "SELECT * FROM illustration WHERE color=? AND category=? AND material=?";
+    connection.query(sql, [req.params.color, req.params.category, req.params.material], (err, results) => {
+        if (err) {
+            res.status(500).send({ errorMessage: 'Cannot get the illustrations' });
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+
+router.post('/', (req, res) => {
+    const sql = "INSERT INTO illustration SET ?";
+    connection.query(sql, req.body, (err, results) => {
+        if (err) {
+            res.status(500).send({ errorMessage: 'Cannot add your illustration' });
+        } else {
+            res.status(201).json({ id: results.insertId, ...req.body });
+        }
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const sql = "DELETE FROM illustration WHERE id=?";
+    connection.query(sql, req.params.id, (err, results) => {
+        if (err) {
+            res.status(500).send({ errorMessage: `Cannot delete this illustration id: ${req.params.id}` });
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
 
 module.exports = router;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const adminRouter = require('./admin.routes.js');
+const galerieRouter = require('./galerie.routes.js');
 
 router.use('/admins', adminRouter);
+router.use('/galerie', galerieRouter);
 
 module.exports = router;
