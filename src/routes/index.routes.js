a04c40181384f7150063c1372b800752d9b31091
lolarne <rne.lola@gmail.com>
2021-02-04 09:44:36 +0100

    add upload

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const adminRouter = require('./admin.routes.js');
 const galerieRouter = require('./galerie.routes.js');
+const uploadRouter = require('./upload.routes.js');
 
 router.use('/admins', adminRouter);
 router.use('/galerie', galerieRouter);
+router.use('/upload', uploadRouter);
 
 module.exports = router;
---
+++ src/routes/upload.routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+router.post("/", (req, res) => {
+    if (req.files === null) {
+        return res.status(400).json({ errorMessage: "No file uploaded" });
+    }
+
+    const file = req.files.file;
+
+    file.mv(
+        `${__dirname}/../../../checkpoint_04_front/public/upload/${file.name}`,
+        (err) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send(err);
+            }
+            res.status(200).json({
+                fileName: file.name,
+                filePath: `/upload/${file.name}`,
+            });
+        }
+    );
+});
+
+module.exports = router;
