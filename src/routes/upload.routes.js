9fa59d9f726cad7904ecd07bf73413d1701804cf
lolarne <rne.lola@gmail.com>
2021-02-04 19:36:44 +0100

    upload works

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,25 +1,24 @@
 const router = require('express').Router();
+const express = require('express');
+const fileUpload = require('express-fileupload');
 
-router.post("/", (req, res) => {
-    if (req.files === null) {
-        return res.status(400).json({ errorMessage: "No file uploaded" });
+router.use(fileUpload());
+
+router.post('/upload', (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ errorMessage: 'No file uploaded' });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`/home/lolaraine/checkpoint04/checkpoint_04_front/public/upload/${file.name}`, err => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(err);
     }
 
-    const file = req.files.file;
+    res.json({ fileName: file.name, filePath: `/upload/${file.name}`});
+  });
 
-    file.mv(
-        `${__dirname}/../checkpoint_04_front/public/upload/${file.name}`,
-        (err) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).send(err);
-            }
-            res.status(200).json({
-                fileName: file.name,
-                filePath: `/upload/${file.name}`,
-            });
-        }
-    );
-});
-
+})
 module.exports = router;
